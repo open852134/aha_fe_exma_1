6069ec69f5220c65cd1fb148dff1c1314063cd61
Steven Weng <open852134@gmail.com>
2023-09-24 15:52:19 +0800

    feat: add calendar component

---
+++ src/components/Calendar/Calendar.tsx
@@ -0,0 +1,101 @@
+import React, { useState, useCallback } from 'react';
+import { formatShortDate } from './helper';
+import DateSection from './DateSection';
+import YearSection from './YearSection';
+
+enum monthActions {
+  PREVIOUS = 'previous',
+  NEXT = 'next',
+}
+
+interface CalendarProps {
+  value: Date | null;
+  defaultValue?: Date;
+  onConfirm: (date: Date) => void;
+  onCancel: () => void;
+}
+
+const Calendar: React.FC<CalendarProps> = ({
+  value,
+  defaultValue = new Date(),
+  onConfirm,
+  onCancel,
+}) => {
+  const [currentDate] = useState(value || defaultValue);
+  const [selectedDate, setSelectedDate] = useState(value || defaultValue);
+  const [renderDate, setRenderDate] = useState(value || defaultValue);
+  const [isShowYearPicker, setIsShowYearPicker] = useState(false);
+
+  const handleMonthChange = useCallback(
+    (action: monthActions) => {
+      const nextRenderDate = new Date(renderDate);
+      nextRenderDate.setDate(1);
+      const month = nextRenderDate.getMonth() + (action === monthActions.PREVIOUS ? -1 : 1);
+      nextRenderDate.setMonth(month);
+
+      setRenderDate(nextRenderDate);
+    },
+    [renderDate],
+  );
+
+  const handleYearChange = useCallback(
+    (newYear: number, newIsShowYearPicker: boolean = true) => {
+      const nextRenderDate = new Date(renderDate);
+      nextRenderDate.setFullYear(newYear);
+      setRenderDate(nextRenderDate);
+      setIsShowYearPicker(newIsShowYearPicker);
+    },
+    [renderDate],
+  );
+
+  return (
+    <div className="relative shadow-aha bg-[#1B1B1B] text-white max-w-[320px] overflow-hidden font-[Inter]">
+      <p className="pt-[17px] pl-[24px] mb-1 text-[16px] leading-6">Text</p>
+      <p className="pl-[24px] text-[32px] leading-[1.375]">
+        {formatShortDate(renderDate)}
+      </p>
+
+      <main>
+        {!isShowYearPicker ? (
+          <DateSection
+            renderDate={renderDate}
+            currentDate={currentDate}
+            selectedDate={selectedDate}
+            onPreviousMonthClick={() => handleMonthChange(monthActions.PREVIOUS)}
+            onNextMonthClick={() => handleMonthChange(monthActions.NEXT)}
+            onYearPickerClick={() => {
+              setIsShowYearPicker(!isShowYearPicker);
+            }}
+            onDateChange={(date) => {
+              setSelectedDate(date);
+            }}
+          />
+        ) : (
+          <YearSection
+            renderDate={renderDate}
+            onNextYearClick={() => handleYearChange(renderDate.getFullYear() + 20)}
+            onPreviousYearClick={() => handleYearChange(renderDate.getFullYear() - 20)}
+            onYearSelect={(year) => handleYearChange(year, false)}
+          />
+        )}
+      </main>
+
+      <section className="mt-3 mb-4 pr-[27px] text-right">
+        <button className="py-2 px-4" type="button" onClick={onCancel}>
+          Cancel
+        </button>
+        <button
+          className="py-2 px-4"
+          type="button"
+          onClick={() => onConfirm(selectedDate)}
+        >
+          OK
+        </button>
+      </section>
+    </div>
+  );
+};
+
+Calendar.defaultProps = { defaultValue: new Date() };
+
+export default Calendar;
---
+++ src/components/Calendar/DateSection.tsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import classNames from 'classnames';
+import { ReactComponent as ArrowLeft } from '@/assets/arrow-left.svg';
+import { ReactComponent as ArrowRight } from '@/assets/arrow-right.svg';
+import { isSameDay, isSameMonth, formatDateToMonthYear } from './helper';
+
+interface DateSectionProps {
+  renderDate: Date;
+  currentDate: Date;
+  selectedDate: Date;
+  onNextMonthClick: () => void;
+  onPreviousMonthClick: () => void;
+  onYearPickerClick: () => void;
+  onDateChange: (date: Date) => void;
+}
+
+const days = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
+const weeks = ['w1', 'w2', 'w3', 'w4', 'w5', 'w6'];
+
+const DateSection: React.FC<DateSectionProps> = ({
+  renderDate,
+  currentDate,
+  selectedDate,
+  onYearPickerClick,
+  onNextMonthClick,
+  onPreviousMonthClick,
+  onDateChange,
+}) => {
+  const getDayByIndexes = (weekIndex: number, dayIndex: number) => {
+    const firstDayOfWeek = new Date(
+      renderDate.getFullYear(),
+      renderDate.getMonth(),
+      1,
+    ).getDay();
+
+    const daysFromPreviousMonth = firstDayOfWeek - dayIndex;
+    const dateOffset = weekIndex * 7 - daysFromPreviousMonth;
+
+    return new Date(
+      renderDate.getFullYear(),
+      renderDate.getMonth(),
+      1 + dateOffset,
+    );
+  };
+
+  return (
+    <section className="calendar-date-picker">
+      <div className="mb-[15px]">
+        <div className="flex justify-between items-center">
+          <button type="button" className="p-3" onClick={onPreviousMonthClick}>
+            <ArrowLeft />
+          </button>
+          <p
+            className="text-base leading-6 cursor-pointer"
+            onClick={onYearPickerClick}
+            aria-hidden="true"
+          >
+            {formatDateToMonthYear(renderDate)}
+          </p>
+          <button type="button" className="p-3" onClick={onNextMonthClick}>
+            <ArrowRight />
+          </button>
+        </div>
+
+        <div className="px-[16px] grid grid-cols-7 gap-x-[6px] mb-3">
+          {days.map((day) => (
+            <span className="text-[11px] text-center text-[#929292]">
+              {day}
+            </span>
+          ))}
+        </div>
+
+        {weeks.map((week, weekIndex) => (
+          <div key={week} className="px-[16px] grid grid-cols-7 gap-x-[6px]">
+            {days.map((day, dayIndex) => {
+              const date = getDayByIndexes(weekIndex, dayIndex);
+              return (
+                <span
+                  key={`${week}-${day}`}
+                  className={classNames(
+                    'cursor-pointer text-sm text-center text-[#929292] h-[36px] leading-[36px] rounded-full hover:bg-white hover:text-[#080808]',
+                    isSameMonth(date, renderDate) && 'text-white',
+                    isSameDay(date, currentDate)
+                      && 'border-[#00A3FF] border-[1px] hover:border-none',
+                    isSameDay(date, selectedDate) && 'bg-[#00A3FF]',
+                  )}
+                  onClick={() => {
+                    onDateChange(date);
+                  }}
+                  aria-hidden="true"
+                >
+                  {date.getDate()}
+                </span>
+              );
+            })}
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default DateSection;
