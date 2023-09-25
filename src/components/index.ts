0088a178b25276f913d6b63a25331a87954e3a57
Steven Weng <open852134@gmail.com>
2023-09-25 22:03:10 +0800

    feat: add calendar input

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { PasswordInput } from './PasswordInput';
 export { Label } from './Label';
 export { Calendar } from './Calendar';
+export { CalendarInput } from './CalendarInput';
--- src/helper/date.ts
+++ src/helper/date.ts
@@ -45,20 +45,13 @@ const formatShortDate = (date: Date) => {
   return `${month}, ${year}`;
 };
 
-const isSameDay = (date1: Date, date2: Date) => (
-  date1.getFullYear() === date2.getFullYear()
+const isSameDay = (date1: Date, date2: Date) => date1.getFullYear() === date2.getFullYear()
   && date1.getMonth() === date2.getMonth()
-  && date1.getDate() === date2.getDate()
-);
+  && date1.getDate() === date2.getDate();
 
-const isSameMonth = (date1: Date, date2: Date) => (
-  date1.getFullYear() === date2.getFullYear()
-  && date1.getMonth() === date2.getMonth()
-);
+const isSameMonth = (date1: Date, date2: Date) => date1.getFullYear() === date2.getFullYear()
+  && date1.getMonth() === date2.getMonth();
 
 export {
-  formatDateToMonthYear,
-  formatShortDate,
-  isSameDay,
-  isSameMonth,
+  formatDateToMonthYear, formatShortDate, isSameDay, isSameMonth,
 };
