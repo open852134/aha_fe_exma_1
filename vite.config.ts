8a0260cff535cbfef609bdd0a3704d846e1d4b7f
Steven Weng <open852134@gmail.com>
2023-09-26 16:19:10 +0800

    feat: add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,28 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import * as path from 'path';
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    sourcemap: true,
+  },
   server: {
     port: 3000,
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: 'vitest.setup.js',
   },
 });
